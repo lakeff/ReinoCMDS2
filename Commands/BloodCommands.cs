8e603242aff0b504bf3ac8af52207f2222078a2d
Odjit <odjiti@gmail.com>
2024-05-25 14:07:59 -0400

    Need to properly handle the case of being unable to add blood merlot to a player's inventory

--- Commands/BloodCommands.cs
+++ Commands/BloodCommands.cs
@@ -18,6 +18,13 @@ internal static class BloodCommands
 		{
 			Entity entity = Helper.AddItemToInventory(ctx.Event.SenderCharacterEntity, new PrefabGUID(1223264867), 1);
 
+			if(entity == Entity.Null)
+			{
+				ctx.Reply($"Received <color=#ff0>{i}</color> Blood Merlot of <color=#ff0>{type}</color> type of <color=#ff0>{quality}</color>% quality");
+				ctx.Reply($"Inventory is full, could not add the last <color=#ff0>{quantity - i}</color> Blood Merlot");
+				return;
+			}
+
 			var blood = new StoredBlood()
 			{
 				BloodQuality = quality,
